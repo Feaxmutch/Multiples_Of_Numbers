2479aeb352c95a04c25b989b0b9fd7f2d0b3608e
Feaxmutch <feaxmutch@gmail.com>
2024-10-06 12:43:58 +0300

    Implemented numbers counting

--- MultiplesOfNumbers/Program.cs
+++ MultiplesOfNumbers/Program.cs
@@ -4,7 +4,26 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Random random = new();
+
+            int minMultiplyer = 10;
+            int maxMultiplyer = 25;
+            int minNumber = 50;
+            int maxNumber = 150;
+
+            int numbersCount = 0;
+            int multiplyer = random.Next(minMultiplyer, maxMultiplyer + 1);
+
+            for (int i = 0; i <= maxNumber; i+= multiplyer)
+            {
+                if (i >= minNumber && i <= maxNumber)
+                {
+                    numbersCount++;
+                }
+            }
+
+            Console.WriteLine($"Количество чисел кратных {multiplyer} = {numbersCount}");
+            Console.ReadKey();
         }
     }
 }
